ec3a924ca1bfbad217fc05a39fa22d88453e5a7e
Mathias <mathm@kth.se>
2021-09-21 21:44:48 +0200

    Add `append_moves` functions for king and pawn

--- chess-engine/src/piece/king.rs
+++ chess-engine/src/piece/king.rs
@@ -1,5 +1,36 @@
 use crate::{Board, Position};
 
-pub fn append_moves(_board: &Board, _from: Position, _dst: &mut Vec<Position>) {
-    unimplemented!()
+pub fn append_moves(board: &Board, from: Position, dst: &mut Vec<Position>) {
+    let color = board[from].unwrap().color;
+    for (x, y) in [
+        (1, 0),
+        (1, -1),
+        (0, -1),
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+    ] {
+        let pos = match Position::new_i8(from.file() as i8 + x, from.rank() as i8 * y) {
+            Some(pos) => pos,
+            None => continue,
+        };
+
+        let target_color = board[pos].map(|p| p.color);
+
+        if target_color == Some(color) {
+            continue;
+        }
+
+        dst.push(pos);
+    }
+
+    if board.can_castle_queenside(color) {
+        dst.push(Position::new_unchecked(from.rank() - 2, from.rank()))
+    }
+
+    if board.can_castle_kingside(color) {
+        dst.push(Position::new_unchecked(from.rank() + 2, from.rank()))
+    }
 }
--- chess-engine/src/piece/pawn.rs
+++ chess-engine/src/piece/pawn.rs
@@ -1,5 +1,24 @@
-use crate::{Board, Position};
+use crate::{Board, Color, Position};
 
-pub fn append_moves(_board: &Board, _from: Position, _dst: &mut Vec<Position>) {
-    unimplemented!()
+pub fn append_moves(board: &Board, from: Position, dst: &mut Vec<Position>) {
+    let color = board[from].unwrap().color;
+    let forwards = color.forwards();
+
+    if let Some(pos) = Position::new(from.file(), (from.rank() as i8 + forwards) as u8) {
+        dst.push(pos);
+    }
+
+    if from.rank() == 6 && color == Color::White || from.rank() == 1 && color == Color::Black {
+        if let Some(pos) = Position::new(from.file(), (from.rank() as i8 + forwards * 2) as u8) {
+            dst.push(pos);
+        }
+    }
+
+    if let Some(en_passant) = board.en_passant_square() {
+        if (from.file() as i8 - en_passant.file() as i8).abs() == 1
+            && en_passant.rank() == (from.rank() as i8 + forwards) as u8
+        {
+            dst.push(en_passant);
+        }
+    }
 }
